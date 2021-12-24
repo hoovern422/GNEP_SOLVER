22b3c0ed24018190151ba23b9fd06b64bf3f90f9
Nickolas Hoover <nick.hoover422@gmail.com>
2021-12-24 17:49:33 -0500

    Updated playing environment

--- training_env.py
+++ training_env.py
@@ -3,44 +3,26 @@ import scipy as sp
 import math
 from scipy.optimize import minimize_scalar
 
-class GNEP_ENV(object):
+class TRAINING_ENV(object):
 
-    def __init__(self, obj_func1, obj_func2, x_bounds, y_bounds, constraint_func):
+    def __init__(self, x_bounds, y_bounds, threshold, reward_scaling_factor):
 
-        self.obj_func1 = obj_func1
-        self.obj_func2 = obj_func2
         self.x_min = x_bounds[0]
         self.x_max = x_bounds[1]
         self.y_min = y_bounds[0]
         self.y_max = y_bounds[1]
-        self.constraint_func = constraint_func
+        self.threshold = threshold
+        self.reward_scaling_factor = reward_scaling_factor
 
         self.cur_x = 0
         self.cur_y = 0
         self.prev_x_opt = 0
         self.prev_y_opt = 0
         self.cur_dist = 1000
-        self.prev_dist = 1000
         self.game_over = False
         self.is_nash_eq = False
 
 ####################################################################################################################
-
-########################################################################################################################
-
-    def findMaxX(self, y):
-        ten_bound = (y < 5)*1
-        sum_bound = (y >= 5)*1
-        maxX = ten_bound*10 + sum_bound*(15-y)
-        return maxX
-
-    def findMaxY(self, x):
-        ten_bound = (x < 5)*1
-        sum_bound = (x >= 5)*1
-        maxY = ten_bound*10 + sum_bound*(15-x) 
-        return maxY
-
-########################################################################################################################
     
     # Generate the environment's response to an action taken by the agent.
 
@@ -54,11 +36,10 @@ class GNEP_ENV(object):
 
             self.cur_y = self.cur_y + action            
 
-        x_opt = self.optimize_func(self.obj_func1, self.cur_y)
-        y_opt = self.optimize_func(self.obj_func2, self.cur_x)
+        x_opt = optimize_x(self.cur_y)
+        y_opt = optimize_y(self.cur_x)
         self.cur_dist = self.get_euclidian_dist(x_opt, y_opt)
         reward = self.get_reward()
-        self.prev_dist = self.cur_dist
 
         if (self.is_valid_move() == False):
 
@@ -73,8 +54,8 @@ class GNEP_ENV(object):
 
     def is_valid_move(self):
 
-        if (self.cur_x < self.x_min or self.cur_x > self.findMaxX(self.cur_y) \
-           or self.cur_y < self.y_min or self.cur_y > self.findMaxY(self.cur_x)):
+        if (self.cur_x < self.x_min or self.cur_x > find_max_x(self.cur_y) \
+           or self.cur_y < self.y_min or self.cur_y > find_max_y(self.cur_x)):
         
             is_valid = False
 
@@ -84,34 +65,7 @@ class GNEP_ENV(object):
 
         return is_valid    
 
-####################################################################################################################
-
-    def optimize_func(self, func_to_opt, other):
-
-        if (func_to_opt == self.obj_func1):
-
-            lim = self.findMaxX(self.cur_y)
-
-            def func_to_min(x):
-                return x**2 + (8/3)*x*other - 34*x
-
-        else:
-
-            lim = self.findMaxY(self.cur_x)
-
-            def func_to_min(y):
-                return y**2 + (5/4)*other*y - 24.25*y       
-
-        if (lim < 1.0e-4):
-            lim = 1.0e-4
-
-        bounds = (0, lim)
-
-        solver = sp.optimize.minimize_scalar(func_to_min, bounds=bounds, method='bounded', tol=None, options=None)
-        opt = solver.x
-
-        return opt
-####################################################################################################################        
+####################################################################################################################       
 
     def get_euclidian_dist(self, x_opt, y_opt):
 
@@ -124,7 +78,7 @@ class GNEP_ENV(object):
 
     def get_reward(self):
 
-        if (self.cur_dist <= 0.1):
+        if (self.cur_dist <= self.threshold):
 
             reward = 0
             self.game_over = True
@@ -132,7 +86,7 @@ class GNEP_ENV(object):
 
         else:
 
-            reward = - self.cur_dist
+            reward = - self.cur_dist * reward_scaling_factor
 
         return reward    
 
@@ -144,7 +98,7 @@ class GNEP_ENV(object):
 
         while (valid == False):
 
-            # Choose a random x coordinate within the constraints
+            # Choose a random x coordinate within the constraints.
             self.cur_x = (self.x_max - self.x_min) * np.random.random_sample() + self.x_min
 
             # Choose a random y coordinate within the constrataints.
@@ -152,9 +106,9 @@ class GNEP_ENV(object):
 
             valid = self.is_valid_move() 
 
-        x_opt = self.optimize_func(self.obj_func1, self.cur_y)
-        y_opt = self.optimize_func(self.obj_func2, self.cur_x)    
-        self.prev_dist = self.get_euclidian_dist(x_opt, y_opt)         
+        x_opt = optimize_x(self.cur_y)
+        y_opt = optimize_y(self.cur_x)    
+        self.cur_dist = self.get_euclidian_dist(x_opt, y_opt)     
 
         self.game_over = False
         self.is_nash_eq = False
@@ -162,5 +116,3 @@ class GNEP_ENV(object):
         new_observation = [self.cur_x, self.cur_y]
 
         return new_observation
-
-####################################################################################################################  
